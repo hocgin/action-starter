b398ff22726ec48293dbbbf32e52995e934d42e1
hocgin <hocgin@gmail.com>
2022-09-12 00:03:52 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,21 @@ import {run} from "./core";
 import * as core from "@actions/core";
 
 export interface Inputs {
+    debug?: boolean;
 }
 
 export interface Outputs {
-
+    // ..
+    [key: string]: any
 }
 
 let getInput = (): Inputs => ({
-    // TODO
+    debug: core.getInput('debug') === 'true'
 })
 
-let handleOutput = (output: Outputs) => {
-    // TODO
+let handleOutput = (output: Outputs = {}) => {
+    Object.keys(output).forEach((key) => core.setOutput(key, output[key]));
+    debugPrintf('输出变量: ', output);
 };
 
 try {
@@ -22,3 +25,8 @@ try {
     core.setFailed(error?.message);
 }
 
+export let debugPrintf = (...args: any) => {
+    if (getInput().debug) {
+        console.log(...args);
+    }
+}
